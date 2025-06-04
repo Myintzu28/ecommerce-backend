d9641aefeb28ee86f22c2e41a5f8d8d5faa132ce
Myint Zu Khin <myintzu@blue-stone.net>
2025-06-04 19:49:17 +0630

    [ADD] json utils

--- src/main/java/com/ecommerce/ecommerce/utils/JsonUtils.java
+++ src/main/java/com/ecommerce/ecommerce/utils/JsonUtils.java
@@ -1,4 +1,31 @@
 package com.ecommerce.ecommerce.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class JsonUtils {
+    public static String toJson(Object object) {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        jsonMapper.findAndRegisterModules();
+        try {
+            return jsonMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            log.error("Failed to serialize object to JSON", e);
+            return "Serialization error";
+        }
+    }
+
+    public static String prettyJson(Object object) {
+        ObjectMapper jsonMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        jsonMapper.findAndRegisterModules();
+
+        try {
+            return jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+        } catch (Exception e) {
+            log.error("Failed to serialize object to JSON", e);
+            return "Serialization error";
+        }
+    }
 }
